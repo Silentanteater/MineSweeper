ea09404c2b6e60c875e497895a310888b3ae20f3
Silentanteater <106014500+Silentanteater@users.noreply.github.com>
2022-05-31 09:38:43 +0300

    update to populateTheBoard function
    an update to add new functionality such as button location classification (determines if the button is on one of the edges of the board)

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-let buttonArray = []
-const board = document.querySelector('.gameBoard')
-for (let i=0; i<64; i++) {
-buttonArray[i] = document.createElement('button');
-buttonArray[i].classList.add('boardButton');
-buttonArray[i].setAttribute('id', i);
-board.appendChild(buttonArray[i])
+function populateTheBoard() {
+  let buttonArray = []
+  let boardSize = 64;
+  let boardSideLength = 8;
+  const board = document.querySelector('.gameBoard')
+  for (let i = 0; i < boardSize; i++) {
+    buttonArray[i] = document.createElement('button');
+    buttonArray[i].classList.add('boardButton');
+    
+    if (i+1<=boardSideLength)
+    {buttonArray[i].classList.add('topEdge');}
+    if (i+1>=boardSize-boardSideLength)
+    {buttonArray[i].classList.add('bottomEdge');}
+    if ((i+1)%boardSideLength==0)
+    {buttonArray[i].classList.add('rightEdge');}
+    if ((i+1)%boardSideLength==1)
+    {buttonArray[i].classList.add('leftEdge');}
+    
+    buttonArray[i].setAttribute('id', i + 1);
+    board.appendChild(buttonArray[i])
+  }
 }
